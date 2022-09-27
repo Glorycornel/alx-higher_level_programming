fd2a7e553f51d2d09be388f3361cbf853353d01b
Glorycornel <104757918+Glorycornel@users.noreply.github.com>
2022-09-28 00:02:16 +0100

    Update 1-main.py

--- 0x0B-python-input_output/Tests/1-main.py
+++ 0x0B-python-input_output/Tests/1-main.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-MyList = __import__('1-my_list').MyList
+write_file = __import__('1-write_file').write_file
 
-my_list = MyList()
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.append(3)
-my_list.append(5)
-print(my_list)
-my_list.print_sorted()
-print(my_list)
+nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+print(nb_characters)
