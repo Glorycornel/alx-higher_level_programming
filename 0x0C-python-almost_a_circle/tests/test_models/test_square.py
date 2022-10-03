a00e322a8b16319874e13cebfb43bafd1b81466d
Glorycornel <104757918+Glorycornel@users.noreply.github.com>
2022-10-03 22:20:46 +0100

    Update test_square.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3>
 """Defines unittests for models/square.py.
+
 Unittest classes:
     TestSquare_instantiation - line 24
     TestSquare_size - line 88
@@ -343,6 +344,7 @@ class TestSquare_stdout(unittest.TestCase):
     @staticmethod
     def capture_stdout(sq, method):
         """Captures and returns text printed to stdout.
+
         Args:
             sq (Square): The Square ot print to stdout.
             method (str): The method to run on sq.
